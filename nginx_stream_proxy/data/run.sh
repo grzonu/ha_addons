35919efd1befdedf5acf6eac69e097a8659ebea6
Filip Grzonkowski <grzonu@gmail.com>
2022-06-04 13:10:13 +0200

    remove sed

--- nginx_stream_proxy/data/run.sh
+++ nginx_stream_proxy/data/run.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bashio
 set -e
 
-ADDRESS=$(bashio::config 'address')
-
-# Prepare config file
-sed -i "s/%%ADDRESS%%/$ADDRESS/g" /etc/nginx.conf
-
 # start server
 bashio::log.info "Running nginx..."
 exec nginx -c /etc/nginx.conf < /dev/null
